8df1a98008e5dd1d5349f848a22758b77db3e764
rabianikwan <rabianikwan67@gmail.com>
2023-07-23 17:29:44 +0800

    feat: settle api routes

--- router/routes.ts
+++ router/routes.ts
@@ -1,5 +1,24 @@
 import { Router } from "express";
+import {
+    createLink,
+    deleteLink,
+    getAllLinks,
+    homepage,
+    searchByLinkName,
+    updateLink, urlShortener
+} from "../controller/routerHandler";
+import Link from "../model/linkHandler";
+import {futimes} from "fs";
 
-const routes = Router()
+const routes = Router();
+const model = new Link()
+const api:string = '/api/v1/link'
 
+routes.get('/', homepage)
+routes.get('/:name', urlShortener)
+routes.get(api, getAllLinks)
+routes.get(api + '/:name', searchByLinkName)
+routes.post(api, createLink)
+routes.delete(api, deleteLink)
+routes.put(api, updateLink)
 export default routes;
